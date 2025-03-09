e8883364973c299137d2c787a2889bdccb8b2952
ThomasLaPiana <thomas.lapiana+github@pm.me>
2025-03-09 17:40:45 +0800

    add more code for the consumer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::time::Duration;
 
 use rdkafka::ClientConfig;
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer};
+use rdkafka::message::{Headers, Message};
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use tokio::io::AsyncWriteExt;
 
@@ -15,35 +18,44 @@ async fn write_to_stdout(text: &str) -> Result<(), Box<dyn ::std::error::Error>>
 async fn main() -> Result<(), Box<dyn ::std::error::Error>> {
     write_to_stdout("Welcome to Kafka Chat!\n").await?;
 
+    // Set up the hardcoded test info
     let url = "localhost:9092";
-    let producer = create_producer(url);
+    let test_payload = "test message";
+    let test_key = "test_key";
+    let test_topic = "test_topic";
 
-    let test_message = "test message";
+    // Create the Producer and Consumer
+    let producer = create_producer(url);
+    let consumer = create_consumer(url);
+    consumer
+        .subscribe(&[test_topic])
+        .expect("Can't subscribe to specified topics!");
 
     // Send the line to Kafka as a producer
-    write_to_stdout("Writing message to Kafka topic...\n").await?;
-    let result = producer
-        .send(
-            FutureRecord::to("chat")
-                .payload(test_message)
-                .key("test_key"),
-            Duration::from_secs(0),
-        )
-        .await;
-
-    // Print the error message if there is one
-    let error_text = result.map_err(|err| format!("{:?}", err));
-    if let Err(text) = error_text {
-        write_to_stdout(&format!(
-            "{}{}\n",
-            "! failed writing to Kafka with error: ", text
-        ))
-        .await?;
-    }
+    tokio::spawn(async move {
+        write_to_stdout("> Writing message to Kafka topic...\n")
+            .await
+            .unwrap();
+        producer
+            .send_result(
+                FutureRecord::to(test_topic)
+                    .payload(test_payload)
+                    .key(test_key),
+            )
+            .unwrap()
+            .await
+            .unwrap()
+            .unwrap();
+        println!("> Message Sent Successfully!");
+    });
 
     // Read the message from Kafka
+    println!("> Retrieving Message...");
 
-    Ok(())
+    loop {
+        let message = consumer.recv().await.unwrap();
+        println!("{:?}", message);
+    }
 }
 
 /// Create a Producer to send messages to Kafka
@@ -53,4 +65,16 @@ fn create_producer(bootstrap_server: &str) -> FutureProducer {
         .set("message.timeout.ms", "5000")
         .create()
         .expect("Failed to create client")
+}
+
+/// Create a Consumer to pull messages from Kafka
+fn create_consumer(bootstrap_server: &str) -> StreamConsumer {
+    ClientConfig::new()
+        .set("bootstrap.servers", bootstrap_server)
+        .set("group.id", "test_group")
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .create()
+        .expect("Consumer creation failed")
 }
